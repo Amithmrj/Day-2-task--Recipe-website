646eb94bcdf55b6a5cf72d71c14e07480d0ad2bb
Amith <amithmr2021011@gmail.com>
2025-02-20 11:25:54 +0530

    form added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const form = document.getElementById('joinform');
+// const submit = document.getElementById('submitButton');
+
+// form.addEventListener('input', function () {
+//   submitButton.disabled = !form.checkValidity();
+// });
