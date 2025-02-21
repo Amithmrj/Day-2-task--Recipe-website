ff5b495ef122bf1efb526d8be102665f7b26bd34
Amith <amithmr2021011@gmail.com>
2025-02-21 14:23:52 +0530

    Form details display

--- js/script.js
+++ js/script.js
@@ -4,3 +4,37 @@
 // form.addEventListener('input', function () {
 //   submitButton.disabled = !form.checkValidity();
 // });
+
+$(document).ready(() => {
+
+    $("#form").submit((e) => {
+        e.preventDefault();
+        const formData = {
+            name: $("#name").val(),
+            phone: $("#phone").val(),
+            email: $("#email").val(),
+            date: $("#date").val()
+        };
+        console.log(formData);
+
+        localStorage.setItem("formData", JSON.stringify(formData));
+        $("#modalform").modal('hide');
+
+        let firstname = formData.name.split(" ")[0];
+        console.log(firstname);
+
+        $("#name1").text(firstname);
+        $("#displayName").text(formData.name);
+        $("#displayPhone").text(formData.phone);
+        $("#displayEmail").text(formData.email);
+        $("#displaydate").text(formData.date);
+        $("#confirm").css('display','block')
+    });
+
+    $("#confirmbtn").click(() => {
+        $("#form").trigger("reset");
+        $("#confirm").hide();
+    });
+
+
+})
